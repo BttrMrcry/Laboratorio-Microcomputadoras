257620ce3ce18e4d07a4bd2564665092252c4a03
BttrMrcry <88600092+BttrMrcry@users.noreply.github.com>
2023-03-15 20:10:56 -0600

    Actualización del pdf de la práctica 1

--- Practica3/ej10/ejercicio10.asm
+++ Practica3/ej10/ejercicio10.asm
@@ -3,6 +3,9 @@ include <p16f877.inc>
 valor1 equ h'21'
 valor2 equ h'22'
 valor3 equ h'23'
+;Aumento de la constante 1
+; Para aumentar el tiempo 
+; entre parpadeos
 cte1 equ h'ff'
 cte2 equ 50h
 cte3 equ 60h
@@ -10,6 +13,8 @@ cte3 equ 60h
 GOTO INICIO
  	ORG 5
 INICIO:
+;Programa el puerto B
+;como salida
 	BSF STATUS,RP0
  	BCF STATUS,RP1
  	MOVLW H'00'
@@ -18,28 +23,37 @@ INICIO:
  	MOVLW B'00000000'
 	MOVWF PORTB
 loop2
-	; ESTADO 1
+; Se definen cada uno de
+; los estados del semáforo.
+; Se limpian todos 
+; los bits del puerto B
+; se prender los bits 
+; necesarios y se llama 
+; a la subrutina de retraso
+
+; ESTADO 1
 	CLRF PORTB
 	BSF PORTB, 6
 	BSF PORTB, 0
 	CALL retardo
-	; ESTADO 2
+; ESTADO 2
 	CLRF PORTB
 	BSF PORTB, 5
 	BSF PORTB, 0
 	CALL retardo
-	; ESTADO 3
+; ESTADO 3
 	CLRF PORTB
 	BSF PORTB, 4
 	BSF PORTB, 2
 	CALL retardo
-	; ESTADO 4
+; ESTADO 4
 	CLRF PORTB
 	BSF PORTB, 4
 	BSF PORTB, 1
  	CALL retardo
-
  	GOTO loop2
+
+;Surutina de retardo
 retardo 
 	MOVLW cte1
  	MOVWF valor1
