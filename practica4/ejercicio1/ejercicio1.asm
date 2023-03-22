12afa6f44755c5737c914f726c3765b7a31b5273
BttrMrcry <88600092+BttrMrcry@users.noreply.github.com>
2023-03-22 13:18:41 -0600

    Ejercicios de la práctica 4

---
+++ practica4/ejercicio1/ejercicio1.asm
@@ -0,0 +1,60 @@
+processor 16f877
+include <p16f877.inc>
+valor1 equ h'21'
+valor2 equ h'22'
+valor3 equ h'23'
+cte1 equ h'ff'
+cte2 equ 50h
+cte3 equ 60h
+ 	ORG 0
+GOTO INICIO
+ 	ORG 5
+INICIO:
+	CLRF PORTA
+	BSF STATUS,RP0
+ 	BCF STATUS,RP1
+ 	MOVLW H'00'
+ 	MOVWF TRISB
+	MOVLW 06H
+	MOVWF ADCON1
+	MOVLW 3FH
+	MOVWF TRISA
+ 	BCF STATUS,RP0
+	MOVLW B'00000000'
+	MOVWF PORTB
+
+	
+	
+loop1:
+	BTFSC PORTA, 0
+	GOTO PRENDER_LEDS
+	GOTO APAGAR_LEDS
+goto loop1	
+
+PRENDER_LEDS:
+	MOVLW B'11111111'
+	MOVWF PORTB
+	GOTO loop1
+
+APAGAR_LEDS
+	CLRF PORTB
+	GOTO loop1
+	
+retardo 
+	MOVLW cte1
+ 	MOVWF valor1
+tres 
+	MOVLW cte2
+	MOVWF valor2
+dos 
+	MOVLW cte3
+	MOVWF valor3
+uno 
+	DECFSZ valor3
+ 	GOTO uno
+	DECFSZ valor2
+	GOTO dos
+	DECFSZ valor1
+	GOTO tres
+	RETURN
+	END
