b7bea39d45d3304ea498613b6bb00769737586f3
BttrMrcry <88600092+BttrMrcry@users.noreply.github.com>
2023-03-22 19:59:27 -0600

    PDF de la práctica 4 y código comentado

--- practica4/ejercicio2/ejercicio2.asm
+++ practica4/ejercicio2/ejercicio2.asm
@@ -10,47 +10,62 @@ cte3 equ 60h
 GOTO INICIO
  	ORG 5
 INICIO:
-	CLRF PORTA
+	; Limpia los bits del puerto A
+	CLRF PORTA 
+	;Selecciona el banco de memoria 1
 	BSF STATUS,RP0
  	BCF STATUS,RP1
+	;Programa el puerto B como salida
  	MOVLW H'00'
  	MOVWF TRISB
+	;Se indica que que se usaran entradas
+	;digitales
 	MOVLW 06H
 	MOVWF ADCON1
+	;Se configura al puerto A como entrada
 	MOVLW 3FH
 	MOVWF TRISA
+	;Se regresa al banco 0
  	BCF STATUS,RP0
 	MOVLW B'00000000'
+	;Se limpia la memoria del puerto B
 	MOVWF PORTB
 
 	
-	
+;En este loop se verifica a que caso se quiere 
+;entrar
 loop1:
+
+	;Verifica si se quiere apagar los leds
 	MOVLW h'00'	
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
 	GOTO APAGAR_LEDS
-
+	;Verifica si se quiere prender todos los
+	;leds
 	MOVLW H'01'
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
 	GOTO PRENDER_LEDS
-
+	;Verifica si se quiere realizar corriento 
+	;a la derecha
 	MOVLW H'02'
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
 	GOTO RIGHT_SHIFT	
-
+	;Verifica si se quiere realizar corrimiento 
+	; al a izquierda
 	MOVLW H'03'
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
 	GOTO LEFT_SHIFT
-
+	; Verifica si se quiere realizar zig-zag.
 	MOVLW H'04'
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
 	GOTO ZIG_ZAG_1
-
+	;Verifica si se quiere comportamiento 
+	;intermitente
 	MOVLW H'05'
 	SUBWF PORTA, W
 	BTFSC STATUS, Z
@@ -58,52 +73,79 @@ loop1:
 
 goto loop1	
 
+;Coloca todos los bits del puerto B en 1
 PRENDER_LEDS:
 	MOVLW B'11111111'
 	MOVWF PORTB
 	GOTO loop1
 
+;Coloca todos los bits del puerto B en 0
 APAGAR_LEDS:
 	CLRF PORTB
 	GOTO loop1
 
+
 RIGHT_SHIFT:
+	;Se prende el ultimo bit del puerto
+	; B
 	MOVLW B'10000000'
 	MOVWF PORTB
+	;Limpia el carry para evitar errores
 	BCF STATUS, C
 LOOP_RS:
+	;Verifica si el contedio del puerto B es cero, 
+	; De ser el caso terminó el corrimiento, por lo
+	; que debe empezar de nuevo.
 	MOVLW H'00'
 	SUBWF PORTB
 	BTFSC STATUS, Z
 	GOTO RIGHT_SHIFT
+	; LLama a retardo para hacer visible la accion
 	CALL retardo
+	;verfica si se quiere acceder a otro modo. Si es 
+	;así regresa al loop principal
 	MOVLW H'02'
 	SUBWF PORTA, W
 	BTFSS STATUS, Z
 	GOTO loop1
+	;limpa el contenido del carry
 	BCF STATUS, C
+	;Realiza un corrimiento a la derecha
 	RRF PORTB
 	
 	goto LOOP_RS
 
 LEFT_SHIFT:
+	;Se prende el primer bit del puerto
+	; B
 	MOVLW B'00000001'
 	MOVWF PORTB
-	;BCF STATUS, C
 LOOP_LS:
+	;Verifica si el contedio del puerto B es cero, 
+	; De ser el caso terminó el corrimiento, por lo
+	; que debe empezar de nuevo.
 	MOVLW H'00'
 	SUBWF PORTB
 	BTFSC STATUS, Z
 	GOTO LEFT_SHIFT
+	; LLama a retardo para hacer visible la accion
 	CALL retardo
+	;verfica si se quiere acceder a otro modo. Si es 
+	;así regresa al loop principal
 	MOVLW H'03'
 	SUBWF PORTA, W
 	BTFSS STATUS, Z
 	GOTO loop1
+	;limpa el contenido del carry
 	BCF STATUS, C
+	;Realiza un corrimiento a la izquierda
 	RLF PORTB
 	goto LOOP_LS
 
+; Es casi identico al corrimento a la derecha, pero
+; en lugar de ejecutarse a si mismo cuando termina
+; el corrimiento, ejecuta a ZIG_ZAG_2 que es casí 
+;identico al corrimiento a la izquierda
 ZIG_ZAG_1:
 	MOVLW B'10000000'
 	MOVWF PORTB
@@ -122,6 +164,10 @@ LOOP_ZZ_1:
 	RRF PORTB
 	goto LOOP_ZZ_1
 
+; Es casi identico al corrimento a la izquierda, pero
+; en lugar de ejecutarse a si mismo cuando termina
+; el corrimiento, ejecuta a ZIG_ZAG_1 que es casí 
+;identico al corrimiento a la derecha
 ZIG_ZAG_2:
 	MOVLW B'00000001'
 	MOVWF PORTB
@@ -140,15 +186,19 @@ LOOP_ZZ_2:
 	RLF PORTB
 	goto LOOP_ZZ_2
 
+;Hace parpadear todos los leds de manera intermitente
 INTERMITENTE:
+	;Verifica si se quiere acceder a otro modo y si es
+	; así regresa al loop principal
 	MOVLW H'05'
 	SUBWF PORTA, W
 	BTFSS STATUS, Z
 	GOTO loop1
-	
+	;Prende todos los leds
 	MOVLW H'FF'
 	MOVWF PORTB
 	CALL retardo
+	;Apaga todos los leds
 	CLRF PORTB
 	CALL retardo
 	goto INTERMITENTE	
