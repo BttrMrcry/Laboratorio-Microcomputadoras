3df90899e011ae39b0e36c0c0a6b477fb8bcc66f
Aldo Navarrete <54006119+aldonavarretefp@users.noreply.github.com>
2023-06-04 15:39:14 -0600

    Update proyecto.ino

--- ProyectoFinal/proyecto/proyecto.ino
+++ ProyectoFinal/proyecto/proyecto.ino
@@ -39,29 +39,28 @@
  https://docs.arduino.cc/learn/electronics/lcd-displays
 
 */
-
-// include the library code:
-
 #include <Wire.h>    // Incluimos la librería Wire.h que establece la comunicación con el protocolo I2C.            
 #include <LiquidCrystal_I2C.h>    // Incluimos la librería para usar la pantalla LCD con el módulo I2C.
-// nnn#include <SoftwareSerial.h>
-#include <Ds1302.h>
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int RST = 6, DAT = 7, clk = 8; 
-const int led = 10;
-const int buzzer = 9;
-bool alarmTriggered = false;
-int alarmHour = 1000;
-int alarmMinute = 1000;
+#include <Ds1302.h> // Incluimos la librería para usar el reloj DS1302.
 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2; // Pines del LCD
+const int RST = 6, DAT = 7, clk = 8;  // Pines del RTC
+
+const int led = 10; // Pin del LED
+const int buzzer = 9; // Pin del buzzer
+bool alarmTriggered = false;  // Variable para saber si la alarma está activada
+int alarmHour = 1000; // Variable para almacenar la hora de la alarma
+int alarmMinute = 1000; // Variable para almacenar el minuto de la alarma
+
+/* Variables para el LCD I2C y el reloj */
 LiquidCrystal_I2C lcd(0x27, 12, 2); 
 Ds1302 rtc(RST, clk, DAT);
-//SoftwareSerial bt(2, 3) ; //RX |TX
+
+/* Prototipos de funciones */
 void setOrReset();
 void alarmTrigger();
 
+/* Arreglo de días de la semana */
 const static char* WeekDays[] =
 {
     "Lunes",
@@ -73,23 +72,23 @@ const static char* WeekDays[] =
     "Domingo"
 };
 
-
+/* Función que se ejecuta para la configuración inicial de la alarma */
 void setup() {
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  pinMode(led, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  // set up the LCD's number of columns and rows:
+  lcd.init(); // Inicializar LCD
+  lcd.backlight(); // Encender luz de fondo
+  lcd.clear(); // Limpiar LCD
+  pinMode(led, OUTPUT); // Configurar pin del LED como salida
+  pinMode(buzzer, OUTPUT);  // Configurar pin del buzzer como salida
+  // Configura el tamaño de la pantalla LCD (12x2)
   lcd.begin(12,2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+  // Inicia la comunicación serial a 9600 baudios
   Serial.begin(9600);
+  // Inicializa el reloj
   rtc.init();
-    // test if clock is halted and set a date-time (see example 2) to start it
-    if (rtc.isHalted())
-    {
-        Serial.println("RTC is halted. Setting time...");
+    /* Testea si el reloj está detenido y configura una fecha y hora para iniciarlo. */
+    if (rtc.isHalted()) {
+        Serial.println("RTC dentenido. Configura el tiempo...");
+        /* Configura la fecha y hora para iniciar el reloj. */
         Ds1302::DateTime dt = {
             .year = 23,
             .month = Ds1302::MONTH_JUN,
@@ -99,81 +98,83 @@ void setup() {
             .second = 0,
             .dow = Ds1302::DOW_TUE
         };
-        rtc.setDateTime(&dt);
-
+        rtc.setDateTime(&dt); // Configura la fecha y hora en el RTC
     }
-
-
-
-
 }
+/* Variable auxiliar para almacenar el último minuto impreso en el LCD. */
 int last_minute = 0;
 void loop() {
     lcd.setCursor(0,0);    // Posiciona la primera letra en el segmento 0 de línea 1 (Se empieza a contar desde 0).            
     // get the current time
-    Ds1302::DateTime now;
-    rtc.getDateTime(&now);
-
-    if (last_minute != now.minute)
-    {
-        lcd.clear();
-        alarmTriggered = false;
-        Serial.println("Puedes configurar la alarma presionando la letra 'C' seguido de enter");
-        last_minute = now.minute;
-
-        Serial.print("20");
-        Serial.print(now.year);    // 00-99
+    Ds1302::DateTime now; // Obtén hora actual
+    rtc.getDateTime(&now);  // Configura la hora actual en el RTC
+    if (last_minute != now.minute) {
+        lcd.clear(); // Limpiar LCD
+        alarmTriggered = false; // Reiniciar alarma
+        Serial.println("Puedes configurar la alarma presionando la letra 'C' seguido de enter"); // Mensaje en consola
+        last_minute = now.minute; // Actualizar minuto
+        Serial.print("20"); // Imprimir año
+        Serial.print(now.year);   // 00-99
         Serial.print('-');
-        if (now.month < 10) Serial.print('0');
+        if (now.month < 10) Serial.print('0');  // Imprimir mes
         Serial.print(now.month);   // 01-12
-        Serial.print('-');
-        if (now.day < 10) Serial.print('0');
+        Serial.print('-');  
+        if (now.day < 10) Serial.print('0');  // Imprimir día
         Serial.print(now.day);     // 01-31
         Serial.print(' ');
         Serial.print(WeekDays[now.dow - 1]); // 1-7
         Serial.print(' ');
-        if (now.hour < 10) Serial.print('0');
+        if (now.hour < 10) Serial.print('0'); // Imprimir hora
         Serial.print(now.hour);    // 00-23
         Serial.print(':');
-        if (now.minute < 10) Serial.print('0');
+        if (now.minute < 10) Serial.print('0'); // Imprimir minuto
         Serial.print(now.minute);  // 00-59
         Serial.print(':');
-        if (now.second < 10) Serial.print('0');
+        if (now.second < 10) Serial.print('0'); // Imprimir segundo
         Serial.print(now.second);  // 00-59
         Serial.println();
     }
+    
+    /* Si el minuto se encuentra entre 0 y 9, imprime un cero al principio. */
     String minuteToPrint;
     if(now.minute < 10){
       minuteToPrint = String("0")+String(now.minute);
     }else{
       minuteToPrint = String(now.minute);
     }
-    
 
+    /* Si la hora se encuentra entre 0 y 9, imprime un cero al principio. */
     String hourToPrint;
     if(now.hour < 10){
       hourToPrint = String("0")+String(now.hour);
     }else{
       hourToPrint = String(now.hour);
     }
-    
+    /* Imprime la hora en el LCD */
     String toPrint = hourToPrint + String(':')+ minuteToPrint;
     lcd.setCursor(0, 0);
     lcd.print(toPrint);
+
+    /* Imprime el dia de la semana en el LCD */
     lcd.setCursor(0, 1);
     lcd.print(WeekDays[now.dow - 1]);
+
+    /* Chequeo constante de reseteo de alarma o de la hora*/
     setOrReset();
     alarmTrigger(now);
+    /* Delay de 100 milisegundos*/
     delay(100);
 }
 
+/* Función que activa la alarma */
 void alarmTrigger(const Ds1302::DateTime &now) {
+  /* Si la alarma no ha sido activada y la hora y minuto coinciden con la alarma, activar alarma */
   if(now.hour == alarmHour && now.minute == alarmMinute && !alarmTriggered) {
-      // Activar buzzer
-      // Prender led
       alarmTriggered = true;
+      /* Activar buzzer*/
+      tone(buzzer, 70);      
+      /* Ciclo PWM Encender Led*/
       Serial.println("Alarma activada");
-      tone(buzzer, 70);
       for(int i = 0; i < 256; i++){
         analogWrite(led, i);
         delay(10);
@@ -181,14 +182,14 @@ void alarmTrigger(const Ds1302::DateTime &now) {
   }else{
     return;
   }
-
-  
 }
 
-
-
+/*
+  Función que chequea si se quiere 
+  configurar la alarma o resetearla.
+*/
 void setOrReset(){
-    
+    /* Variable que almacena la opción ingresada por el usuario */
     String option;
     if(Serial.available() > 0){
       option = Serial.readString();
@@ -196,25 +197,32 @@ void setOrReset(){
     }else{
       return;
     }
+    /* La tecla 'C' configura la alarma */
     if(option.equals("C") || option.equals("c")){
+      
       Serial.println("Escribe la hora de la alarma en el formato hh:mm");
-      while(Serial.available() == 0);
+      while(Serial.available() == 0); // Esperar a que se ingrese la hora
+      
+      /* Formateo de la función para imprimir en la terminal serial */
       String userInput = Serial.readString();
       userInput.trim();
       int indexColon = userInput.indexOf(':');
       String hourString = userInput.substring(0, indexColon);
       String minuteString = userInput.substring(indexColon + 1, userInput.length());
+
+      /* Conversión de string a int (FORMATEO) */
       int hourInt = hourString.toInt();
       int minuteInt = minuteString.toInt();
       alarmHour = hourInt;
       alarmMinute = minuteInt;
 
-      
+      /* Imprimir en la terminal serial la hora de la alarma */
       Serial.println(String("Alarma configurada a las ") + String(alarmHour) +String(":")+String(alarmMinute) + String(" exitosamente."));
     }
+
+    /* La tecla 'X' resetea la alarma */
     if(option.equals("X") || option.equals("x")){
-      analogWrite(led, 0);
-      noTone(buzzer);
-      
+      analogWrite(led, 0); // Apagar led
+      noTone(buzzer); // Apagar buzzer
     }
 }
